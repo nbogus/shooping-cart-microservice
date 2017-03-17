8e475995ee7ae0e2070fe8a0a4d7b4c86d42520f
nbogusiewicz <nbogusiewicz@infusion.com>
2017-03-17 17:34:09 +0100

    initial project

---
+++ ShoppingCart.Microservice/src/ShoppingCart.Microservice/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Nancy.Owin;
+
+namespace ShoppingCart.Microservice
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseOwin(buildFunc => buildFunc.UseNancy());
+        }
+    }
+}
